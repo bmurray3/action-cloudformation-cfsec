b463f953928c71da3855e354d97e5d798f06f5c5
Brian Murray <bmurray3@gmail.com>
2022-02-07 15:17:47 -0500

    adding action

---
+++ cfsec.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import subprocess
+
+def write_file(content):
+  file_name = "cfsec.log"
+  with open(file_name, "w") as file:
+    print(f"writing cfsec log to {file_name} ...")
+    file.write(content)
+
+try:
+  output = subprocess.run(
+    [f"cfsec --nocolor ."],
+    check=True,
+    capture_output=True,
+    text=False,
+    shell=True
+  )
+  print(output.stdout.decode("utf8"))
+  write_file(output.stdout.decode("utf8"))
+except subprocess.subprocess.CalledProcessError as error:
+  print(error.stdout.decode("utf8"))
+  print(error.stderr.decode("utf8"))
+  write_file(error.stdout.decode("utf8"))
+  sys.exit(error.returncode)
